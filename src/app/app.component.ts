03ea3fcbe0859594731e96c21b42aa0b5d1e013c
matuzalema <matuzalema@gmail.com>
2020-02-27 03:23:37 +0100

    Add figures and board highlight

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,17 @@ import { getLocaleDateFormat } from '@angular/common';
 })
 export class AppComponent {
   title = 'chees';
+  xValue = 0;
+  yValue = 5;
+  choosenFigure = '';
+  // figures
+  king = '../assets/images/king1.png';
+  hetman = '../assets/images/hetman.png';
+  horse = '../assets/images/horse.png';
+  pawn = '../assets/images/pawn.png';
+  runner = '../assets/images/runner.png';
+  towen = '../assets/images/tower1.png';
+
 
   arr: Tile[][] = [];
 
@@ -15,12 +26,27 @@ export class AppComponent {
     for (let i = 0; i < 8; i++) {
       this.arr[i] = [];
       for (let j = 0; j < 8; j++) {
-        const tile = new Tile(i, j, false, '', false);
+        const tile = new Tile(i, j, false, '', false, '', '');
         this.arr[i][j] = tile;
       }
     }
-    this.arr[5][5].hightlight = true;
+    this.arr[this.xValue][this.yValue].hightlight = true;
     console.log(this.arr);
+  }
+
+  getXValue(event) {
+    this.xValue = event.target.value;
+  }
+  getYValue(event) {
+    this.yValue = event.target.value;
+  }
+  onSubmit() {
+    this.arr[this.xValue][this.yValue].hightlight = true;
+    this.arr[this.xValue][this.yValue].choosenFigure = this.choosenFigure;
+  }
+
+  getFigure(figure) {
+    this.choosenFigure = figure;
   }
 }
 
@@ -30,6 +56,8 @@ export class Tile {
     public y: number,
     public hightlight: boolean,
     public figure: string,
-    public choosen: boolean
+    public choosen: boolean,
+    public choosenFigure: string,
+    public imageSrc: string
   ) {}
 }
--- src/app/cheesboard/cheesboard.component.ts
+++ src/app/cheesboard/cheesboard.component.ts
@@ -7,15 +7,32 @@ import {Tile} from '../app.component';
   styleUrls: ['./cheesboard.component.scss']
 })
 export class CheesboardComponent implements OnInit {
-  arrayTile;
-
-  @Input() arr;
+  @Input() arr: Tile[][];
   constructor() { }
 
   ngOnInit(): void {
+    this.addHightlight(this.arr[2][3]);
   }
+    addHightlight(item: Tile) {
+      return item.hightlight = true;
+    }
 
   getColor(item: Tile): string {
-    return (item.x + item.y) % 2 === 0 ? '#F0D9B5' : '#B58863';
+    if (item.hightlight) {
+      return '#255F69';
+    } else if ((item.x + item.y) % 2 === 0) {
+      return '#ddbca1';
+    } else if ((item.x + item.y) % 2 !== 0) {
+      return '#B58863';
+    }
+  }
+  showDataOfClicledTile(item: Tile) {
+    console.log('Kliknięty tile: ' + 'x: ' + item.x + '  y: ' + item.y);
+    item.hightlight = true;
+
+    // this.arr[item.x ][item.y + 1].hightlight = true;
+    // this.arr[item.x][item.y - 1].hightlight = true;
+    // this.arr[item.x - 1][item.y ].hightlight = true;
+    // this.arr[item.x + 1][item.y].hightlight = true;
   }
 }
